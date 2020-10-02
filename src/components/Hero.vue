629af3e461919bad2af040cb31a75391489ed481
Hector del Angel <hectordelangel@gmail.com>
2020-10-02 12:56:52 -0700

    Add main section to homepage

---
+++ src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class=" -mt-24 w-full bg-cover bg-center h-screen " :style="{'background-image': `url(${require('@/assets/img/guitar_bg.jpg')})`}">
+    <div class="flex w-full h-screen" style="background-color:rgba(0,0,0,0.6)">
+      <div class="container flex flex-col items-center mx-auto">
+        <div class="text-5xl sm:text-8xl font-title text-center text-green-400 mb-2 pt-64">
+          David Montes
+        </div>
+        <div class=" mb-2">
+          <p class="font-sans leading-tight text-3xl text-gray-100">
+            Guitarist
+          </p>
+        </div>
+        <p class="text-base text-center text-gray-100 mt-4 mx-16">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae tenetur vero ducimus voluptatibus dolore quo esse illo laborum sint quasi?
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
