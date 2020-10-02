8296c8caf6bb86a934726d2ff36d0d960c47e0f1
Hector del Angel <hectordelangel@gmail.com>
2020-10-02 12:36:01 -0700

    Init commit, add tailwindcss and google fonts

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      sans: ["Niramit", "Sans-serif"],
+      title: ["David Libre", "serif"],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
